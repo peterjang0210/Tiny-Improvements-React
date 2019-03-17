c060c14a0d2d7cee7e3300cad65c26b8d2ba93eb
Peter Jang <peter.jang@outlook.com>
2019-03-16 20:33:00 -0400

    wireframing of all components

---
+++ tiny-improvements-react/Client/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = (props) => (
+    <div></div>
+)
+
+export default Footer;
---
+++ tiny-improvements-react/Client/src/components/Kudos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Kudos = (props) => (
+    <div></div>
+)
+
+export default Kudos;
